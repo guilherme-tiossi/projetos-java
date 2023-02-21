8e7362ac863f6530d83393433ec79318d3f435a1
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2023-02-21 15:24:01 -0300

    Finalizado exercício de ArrayList2d com calendário escolar

--- primeirosExercícios/src/ArrayList2D.java
+++ primeirosExercícios/src/ArrayList2D.java
@@ -3,15 +3,19 @@ import java.util.*;
 public class ArrayList2D{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        ArrayList<ArrayList<String>> semana = new ArrayList<ArrayList<String>>(); 
+        ArrayList<ArrayList<String>> aulasSemanais = new ArrayList<ArrayList<String>>(); 
         ArrayList<String> dias = new ArrayList<String>(Arrays.asList("Segunda", "Terça", "Quarta", "Quinta", "Sexta"));
-        for(int i = 0; i < 5; i++){
-            ArrayList<String> dia = new ArrayList<String>(); 
+
+        for(int i = 0; i < 5; i++){ //laço for para cada dia da semana
+            ArrayList<String> aulasDia = new ArrayList<String>(); //array usado para armazenar temporariamente as aulas do dia antes de ser passado para o array aulasSemanais
             for(int j = 0; j < 6; j++){
-                String materia = sc.next();
-                dia.add(materia);
+                int k = j + 1; //contador usado para conseguir o valor que precede o indicador ordinal(ª) (3ª matéria de X dia)
+                System.out.println("Digite a " + k + "ª matéria de " + dias.get(i));
+                String materia = sc.nextLine();
+                aulasDia.add(materia);
+                k = 0; //reinicializando o contador k para o próximo dia
             }
-            semana.add(dia);
+            aulasSemanais.add(aulasDia);
         }
 
         for(int i = 0; i < 5; i++){
@@ -19,11 +23,11 @@ public class ArrayList2D{
         }
         System.out.println("\b");
         for(int j = 0; j < 6; j++){
-            System.out.print(semana.get(0).get(j) + " | ");
-            System.out.print(semana.get(1).get(j) + " | ");
-            System.out.print(semana.get(2).get(j) + " | ");
-            System.out.print(semana.get(3).get(j) + " | ");
-            System.out.print(semana.get(4).get(j) + " | ");
+            System.out.print(aulasSemanais.get(0).get(j) + " | "); //pega o primeiro valor do dia J
+            System.out.print(aulasSemanais.get(1).get(j) + " | ");
+            System.out.print(aulasSemanais.get(2).get(j) + " | ");
+            System.out.print(aulasSemanais.get(3).get(j) + " | ");
+            System.out.print(aulasSemanais.get(4).get(j) + " | ");
             System.out.println("\b");
         }
         sc.close();
